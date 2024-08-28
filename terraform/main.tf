9e43078282f24b4c193c8a6791a584b63f2e7297
tdagallart <arturo.gallart@teladochealth.com>
2024-08-28 17:22:19 -0600

    updating terraform code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,77 @@
+# main.tf
+
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
+}
+# Backend configuration
+terraform {
+  backend "s3" {
+    bucket = "tfstatelab "
+    key    = "statefile/terraform.tfstate"  # Change the path as needed
+    region = "us-east-2"  # Change to your preferred region
+  }
 }
 
-resource "aws_vpc" "main" {
+# Create a VPC
+resource "aws_vpc" "main_vpc" {
   cidr_block = "10.0.0.0/16"
+  
   tags = {
     Name = "main_vpc"
   }
+}
+
+# Create public subnets
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "public_subnet_1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "public_subnet_2"
+  }
+}
+
+# Create an Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "main_igw"
+  }
+}
+
+# Create a Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public_route_table"
+  }
+}
+
+# Associate the Route Table with the Subnets
+resource "aws_route_table_association" "public_subnet_1_assoc" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_subnet_2_assoc" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
 }
